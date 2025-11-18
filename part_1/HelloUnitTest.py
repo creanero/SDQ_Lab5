8ec709a03ea76ec2b8af84e7a98765551e16ff58
creanero <creanero@gmail.com>
2025-11-18 12:42:51 +0000

    fixed whitespace bug from Word

--- part_1/HelloUnitTest.py
+++ part_1/HelloUnitTest.py
@@ -3,6 +3,6 @@ import hello #import main file
 
 class TestCalc(unittest.TestCase):
 
-def test_add(self): #all tests have to start with test_ for the program to recognise a test has been created
+    def test_add(self): #all tests have to start with test_ for the program to recognise a test has been created
         result = hello.add(10, 5)
         self.assertEqual(result, 15) #prove that the answer should equal 15
